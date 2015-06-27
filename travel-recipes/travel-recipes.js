fc18a7ba0ac9450091dc547f576a11b04de1d0bf
Michelle <michellegeek2@gmail.com>
2015-06-27 15:33:42 -0700

    Added bootstrap theme

--- travel-recipes/travel-recipes.js
+++ travel-recipes/travel-recipes.js
@@ -2,13 +2,19 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Template.body.helpers({
+    recipes:[
+      {location: "Yosemite", name: "Yosemite", duration:"3", mainImage: "http://www.yosemitepark.com/Images/home-img-01.jpg", summary: "Yoemite high-country trip"},
+       {location: "Niagara Falls", name: "Niagara Falls", duration:"5", mainImage: "http://www.yosemitepark.com/Images/home-img-01.jpg", summary: "Niagara Falls high-country trip"},
+       {location: "Las Vegas", name: "Las Vegas", duration:"3", mainImage: "http://www.yosemitepark.com/Images/home-img-01.jpg", summary: "Las Vegas high-country trip"},
+       {location: "California", name: "California", duration:"3", mainImage: "http://www.yosemitepark.com/Images/home-img-01.jpg", summary: "California high-country trip"},
+       {location: "New York", name: "New York", duration:"3", mainImage: "http://www.yosemitepark.com/Images/home-img-01.jpg", summary: "New York high-country trip"},
+
+
+    ]
   });
 
-  Template.hello.events({
+  Template.body.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
