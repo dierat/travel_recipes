30534d35909fc66addac366911a011e0a05ba9eb
Danielle Gellis <danisyellis@gmail.com>
2015-06-27 15:45:30 -0700

    upgrade metor and fix objects in collections by inserting each object separately

--- travel-recipes/collection.js
+++ travel-recipes/collection.js
@@ -11,14 +11,16 @@ if (Meteor.isServer) {
 				mainImage: "http://1af60cd74e95fe387bc8-1bfee98aeb105b45275a9419b6310abb.r63.cf1.rackcdn.com/164/2/large.jpg",
 				summary: "This will be so cool!",
 				ingredients: ["ing1", "ing2", "ing3"]
-			}, {
+			}); 
+			Recipes.insert({
 				name: "Our Trip",
 				location: "Australia",
 				duration: 24,
 				mainImage: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSu70QunAdWz8tA0NulCGphvFhvipWDQFv34hw6ncdo6GFvaXjL",
 				summary: "All-over Austrailia",
 				ingredients: ["ing1", "ing2", "ing3"]
-			}, {
+			}); 
+			Recipes.insert({
 				name: "Backpacking",
 				location: "Yosemite",
 				duration: 6,
@@ -35,14 +37,16 @@ if (Meteor.isServer) {
 				cost: 85,
 				address: "123 Main St., Paris, France",
 				website: "www.grandhotel.com"
-			}, {
+			});
+			Ingredients.insert({
 				name: "wine",
 				type: "activity",
 				duration: 4,
 				cost: 95,
 				address: "223 Main St., Paris, France",
 				website: "www.champagne.com"
-			}, {
+			});
+			Ingredients.insert({
 				name: "taxi",
 				type: "transportation",
 				duration: 1,
