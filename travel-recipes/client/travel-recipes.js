4d88d7830f04294ae75e4f8770616c66afd5262c
dierat <diedra.rater@gmail.com>
2015-10-28 22:29:30 -0700

    refactors code to be cleaner

--- travel-recipes/client/travel-recipes.js
+++ travel-recipes/client/travel-recipes.js
@@ -1,23 +1,9 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Template.travelRecipes.helpers({
+  recipes: () => Recipes.find({}, {sort: {dateCreated: -1}})
+});
 
-  Template.travelRecipes.helpers({
-      recipes: function(){
-        return Recipes.find({}, {sort: {dateCreated: -1}})
-      }
-  });
-
-  Template.registerHelper(
-     'dateCreatedFromNow', function() {
-      return moment(this.dateCreated).fromNow();
-  })
-
-
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Template.recipe.helpers({
+  dateCreatedFromNow: function(){
+    return moment(this.dateCreated).fromNow();
+  }
+});
