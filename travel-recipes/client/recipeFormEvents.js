1f9d14f37be0f2643dcec35db5decab0794825e6
dierat <diedra.rater@gmail.com>
2015-10-30 13:56:38 -0500

    cleans up code and updates to ecma6

--- travel-recipes/client/recipeFormEvents.js
+++ travel-recipes/client/recipeFormEvents.js
@@ -1,45 +1,51 @@
-if (Meteor.isClient) {
-	Template.recipeForm.events({
-		"click #submitRecipe": function(event) {
-			event.preventDefault();
-			var recipeId = Recipes.insert({
-				name: $("input[name='name']").val(),
-				location: $("input[name='location']").val(),
-				duration: $("input[name='duration']").val(),
-				mainImage: $("input[name='mainImage']").val(),
-				summary: $("textarea[name='summary']").val(),
-				dateCreated: new Date(),
-				userName: Meteor.user().username
-			}); 
-			
-			Router.go("/recipes/" + recipeId)
-			return false
-		}
-	}),
-	Template.ingredientForm.events({
-		"click #submitIngredient": function(event) {
-			event.preventDefault();
-			var recipeId = Router.current().params._id
-			var selected = document.getElementById("ingredientType");
-			var ingredientId = Ingredients.insert({
-				type: selected.options[selected.selectedIndex].value,
-				name: $("input[name='name']").val(),
-				cost: $("input[name='cost']").val(),
-				duration: $("input[name='duration']").val(),
-				address: $("input[name='address']").val(),
-				ingredientPic: $("input[name='ingredientPic']").val(),
-				website: $("input[name='website']").val()
-			})
-			$("input[name='name']").val('');
-			$("input[name='cost']").val('');
-			$("input[name='duration']").val('');
-			$("input[name='address']").val('');
-			$("input[name='ingredientPic']").val('');
-			$("input[name='website']").val('');
-			Recipes.update( {_id: recipeId},
-							{$addToSet: {ingredients: ingredientId}}
-			)
-		}
-	})
-}
+Template.recipeForm.events({
+	"click #submitRecipe": (event) => {
+		event.preventDefault();
 
+		const recipeId = Recipes.insert({
+			name: $("input[name='name']").val(),
+			location: $("input[name='location']").val(),
+			duration: $("input[name='duration']").val(),
+			mainImage: $("input[name='mainImage']").val(),
+			summary: $("textarea[name='summary']").val(),
+			dateCreated: new Date(),
+			userName: Meteor.user().username
+		});
+
+		Router.go("/recipes/" + recipeId);
+		return false;
+	}
+});
+
+
+
+Template.ingredientForm.events({
+	"click #submitIngredient": function(event) {
+		event.preventDefault();
+
+		const recipeId = Router.current().params._id;
+		const selected = document.getElementById("ingredientType");
+		
+		const ingredientId = Ingredients.insert({
+			type: selected.options[selected.selectedIndex].value,
+			name: $("input[name='name']").val(),
+			cost: $("input[name='cost']").val(),
+			duration: $("input[name='duration']").val(),
+			address: $("input[name='address']").val(),
+			ingredientPic: $("input[name='ingredientPic']").val(),
+			website: $("input[name='website']").val()
+		});
+
+		$("input[name='name']").val('');
+		$("input[name='cost']").val('');
+		$("input[name='duration']").val('');
+		$("input[name='address']").val('');
+		$("input[name='ingredientPic']").val('');
+		$("input[name='website']").val('');
+
+		Recipes.update(
+			{_id: recipeId},
+			{$addToSet: {ingredients: ingredientId}}
+		);
+	}
+});
