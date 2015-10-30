affa8f2cf29566a9f31476e5f225d65e8f4f47a6
dierat <diedra.rater@gmail.com>
2015-10-29 19:23:12 -0500

    reformats updateRecipes to not go off the page

--- travel-recipes/server/meteorMethods.js
+++ travel-recipes/server/meteorMethods.js
@@ -1,6 +1,9 @@
 Meteor.methods({
   updateRecipes: function() {
-    Recipes.update({}, {$set: {ingredients: _.pluck(Ingredients.find().fetch(), "_id")}}, {multi: true});
+    Recipes.update(
+      {},
+      {$set: {ingredients: _.pluck(Ingredients.find().fetch(), "_id")}},
+      {multi: true});
   },
   deleteRecipe: function(id){
     Recipes.remove(id);
