c78389496feb7188067c95df64ae6368ab8c3097
dierat <diedra.rater@gmail.com>
2015-11-14 17:26:28 -0800

    shows default trip icon for recipe summary if no image was given

--- travel-recipes/client/recipeViews/recipeList.js
+++ travel-recipes/client/recipeViews/recipeList.js
@@ -2,8 +2,12 @@ Template.travelRecipes.helpers({
   recipes: () => Recipes.find({}, {sort: {dateCreated: -1}})
 });
 
-Template.recipe.helpers({
+Template.recipeSummary.helpers({
   dateCreatedFromNow: function(){
     return moment(this.dateCreated).fromNow();
+  },
+
+  recipeImage: function() {
+    return this.mainImage || "tripicon.png";
   }
 });
